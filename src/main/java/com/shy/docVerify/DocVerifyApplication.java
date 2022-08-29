92e75d67e81e6d62244a07f31ce783214b41e080
hyeokjinNa <hyeokjin.na@vtw.co.kr>
2022-08-29 19:55:56 +0900

    shy ->  DocVerify 프로젝트 변경(패키지, 그룹아이디, 아트팩트아이디 등)

--- src/main/java/com/shy/docVerify/DocVerifyApplication.java
+++ src/main/java/com/shy/docVerify/DocVerifyApplication.java
@@ -1,13 +1,13 @@
-package com.vtw.shy;
+package com.shy.docVerify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShyApplication {
+public class DocVerifyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShyApplication.class, args);
+		SpringApplication.run(DocVerifyApplication.class, args);
 	}
 
 }
--- src/main/java/com/shy/docVerify/controller/MainController.java
+++ src/main/java/com/shy/docVerify/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.vtw.shy.controller;
+package com.shy.docVerify.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
