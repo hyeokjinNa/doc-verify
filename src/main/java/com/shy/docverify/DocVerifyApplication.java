cd3a2cc047259df3f675e12734a1f237eb4d2f42
User <User@LAPTOP-GJA05H47>
2022-08-30 19:08:02 +0900

    doc-verify 프로젝트명 수정

--- src/main/java/com/shy/docverify/DocVerifyApplication.java
+++ src/main/java/com/shy/docverify/DocVerifyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DocverifyApplication {
+public class DocVerifyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DocverifyApplication.class, args);
+		SpringApplication.run(DocVerifyApplication.class, args);
 	}
 
 }
--- src/test/java/com/shy/docverify/DocVerifyApplicationTests.java
+++ src/test/java/com/shy/docverify/DocVerifyApplicationTests.java
@@ -1,10 +1,10 @@
-package com.vtw.shy;
+package com.shy.docverify;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ShyApplicationTests {
+class DocVerifyApplicationTests {
 
 	@Test
 	void contextLoads() {
