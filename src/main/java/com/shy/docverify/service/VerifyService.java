454196f8e12081adafb14c19a22c217d7e123104
양준영 <qsa741@gmail.com>
2022-09-26 20:28:23 +0900

    테이블 검증 기능 추가

--- src/main/java/com/shy/docverify/service/VerifyService.java
+++ src/main/java/com/shy/docverify/service/VerifyService.java
@@ -1,11 +1,14 @@
 package com.shy.docverify.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.shy.docverify.dto.TableDTO;
 
 public interface VerifyService {
 
 	public List<TableDTO> getDBTable(String schema, String table); 
+	
+	public Map<String, Object> verifyTables(List<TableDTO> firstTable, List<TableDTO> secondTable);
 	
 }
--- src/main/java/com/shy/docverify/service/VerifyServiceImpl.java
+++ src/main/java/com/shy/docverify/service/VerifyServiceImpl.java
@@ -1,9 +1,11 @@
 package com.shy.docverify.service;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.codec.binary.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +22,84 @@ public class VerifyServiceImpl implements VerifyService {
 	@Override
 	public List<TableDTO> getDBTable(String schema, String table) {
 		return dbInfoSql.selectTableInfo(schema, table);
+	}
+	
+	@Override
+	public Map<String, Object> verifyTables(List<TableDTO> firstTable, List<TableDTO> secondTable) {
+
+		String[] tableKeys = {"TableName", "PhysicalName", "EntityName", "LogicalName", "DataType", "Length",
+				"Precision", "Scale", "NotNull", "Pk" };
+
+		Map<String, Object> result = new HashMap();
+
+		for (int i = 0; i < firstTable.size(); i++) {
+
+			List<String> firstRow = setRowList(firstTable.get(i));
+
+			boolean check = false;
+			
+			for (int j = 0; j < secondTable.size(); j++ ) {
+
+				List<String> secondRow = setRowList(secondTable.get(j));
+
+				if (StringUtils.equals(firstRow.get(0), secondRow.get(0))
+						&& StringUtils.equals(firstRow.get(1), secondRow.get(1))) {
+					
+					check = true;
+					boolean match = true;
+					
+					firstTable.get(i).setCheck(check);
+					secondTable.get(j).setCheck(check);
+
+					for (int col = 2; col < firstRow.size(); col++) {
+						
+						if (!StringUtils.equals(firstRow.get(col), secondRow.get(col))) {
+							
+							match = false;
+
+							List<String> wrongColumns = firstTable.get(i).getWrongColumns();
+
+							wrongColumns.add(tableKeys[i]);
+							
+							firstTable.get(i).setWrongColumns(wrongColumns);
+							secondTable.get(j).setWrongColumns(wrongColumns);
+						}
+						
+					}
+					
+					firstTable.get(i).setMatch(match);
+					secondTable.get(j).setMatch(match);
+					
+					
+					break;
+				}
+
+			}
+			
+		}
+		
+		result.put("DOC", firstTable);
+		result.put("DB", secondTable);
+
+		return result;
+	}
+	
+	public List<String> setRowList(TableDTO dto) {
+
+		List<String> list = new ArrayList();
+
+		list.add(dto.getTableName());
+		list.add(dto.getPhysicalName());
+		list.add(dto.getEntityName());
+		list.add(dto.getLogicalName());
+		list.add(dto.getDataType());
+		list.add(dto.getLength());
+		list.add(dto.getPrecision());
+		list.add(dto.getScale());
+		list.add(dto.getNotNull());
+		list.add(dto.getPk());
+
+		return list;
 	}
 
 }
